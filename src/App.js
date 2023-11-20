0e663a9f83853944641b47e9f82fb10f184b0475
KaengEE <yopuiqwer@naver.com>
2023-11-20 10:09:03 +0900

    카드 이미지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong> 4000
+          </li>
+          <li>
+            <strong>Downloads: </strong> 300
+          </li>
+          <strong>Likes: </strong> 400
+        </ul>
+      </div>
+      <div className="px-6 py-4 text-sm font-semibold text-gray-700 ">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1  mr-2">
+          #tag3
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
