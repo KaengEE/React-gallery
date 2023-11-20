31ab397d4a2264691494df8d042913cfaf266c44
KaengEE <yopuiqwer@naver.com>
2023-11-20 12:22:56 +0900

    검색기능

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   //api요청
   const [images, setImages] = useState([]); //초기값 빈 배열
-  const [isLoading, setIsLoading] = useState(true); //초기값 true
   const [term, setTerm] = useState("flowers"); //초기 검색값
   //console.log(process.env.REACT_APP_PIXABAY_API_KEY);
 
@@ -16,10 +16,11 @@ function App() {
       .then((res) => res.json()) //결과를 json으로 변환
       .then((data) => setImages(data.hits)) //hits의 데이터
       .catch((err) => console.log(err)); // err 콘솔
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto my-7">
+      <ImageSearch setTerm={setTerm} />
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
         {images.map((image) => (
           <ImageCard key={image.id} image={image} />
---
+++ src/components/ImageSearch.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const ImageSearch = ({ setTerm }) => {
+  //검색어
+  const [text, setText] = useState(""); //초기값 공백
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //두단어이상 조합시 + 해야함 : 앞뒤 공백은 없애고 가운데 공백은 + 로 변경
+    setTerm(text.trim().replace(" ", "+"));
+  };
+  return (
+    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+      <form onSubmit={onSubmit}>
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="이미지 검색..."
+            required
+          />
+          <button
+            type="submit"
+            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            검색
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ImageSearch;
