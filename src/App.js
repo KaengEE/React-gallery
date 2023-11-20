f62d53b2c3e43e1a084971bab42e8f2c9b609eee
KaengEE <yopuiqwer@naver.com>
2023-11-20 10:51:24 +0900

    api 요청

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  //api요청
+  const [images, setImages] = useState([]); //초기값 빈 배열
+  const [isLoading, setIsLoading] = useState(true); //초기값 true
+  const [term, setTerm] = useState("flowers"); //초기 검색값
+  //console.log(process.env.REACT_APP_PIXABAY_API_KEY);
+
+  useEffect(() => {
+    //사진 가져오기
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json()) //결과를 json으로 변환
+      .then((data) => setImages(data.hits)) //hits의 데이터
+      .catch((err) => console.log(err)); // err 콘솔
+  }, []);
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
