f0a2da9098772205a3da92291800b7e7ae9bbb8f
KaengEE <yopuiqwer@naver.com>
2023-11-20 12:31:06 +0900

    색상변경

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -46,7 +46,7 @@ const ImageSearch = ({ setTerm }) => {
           />
           <button
             type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white absolute end-2.5 bottom-2.5 bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
           >
             검색
           </button>
